820d51c488992bcd825d8432b330257dca8de36b
Himanshu Bedi <bedihimanshu>
2023-08-02 23:21:13 +0530

    Minor project completed

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { ImageUpload } from "./home";
+import Nav from "./components/Nav";
+import Diseases from "./components/Diseases";
+import About from "./components/About";
+import { Route, Routes ,BrowserRouter } from 'react-router-dom';
+
+function App() {
+
+  return <> 
+  {/* <Nav/> */}
+  <ImageUpload />
+ 
+  </>;
+}
+
+export default App;
